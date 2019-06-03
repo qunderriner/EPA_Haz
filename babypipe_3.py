5e93559698668050c903e11396db0ed7d4e13fbb
Quinn Underriner <underriner@uchicago.edu>
2019-06-03 14:21:34 -0500

    plot

--- babypipe_3.py
+++ babypipe_3.py
@@ -26,7 +26,8 @@ from sklearn import ensemble
 from sklearn import neighbors
 from sklearn.grid_search import ParameterGrid
 from datetime import timedelta
-
+import matplotlib.pyplot as plt
+from scipy import optimize
 #dictionary of models and parameters - inspiration from 
 #https://github.com/rayidghani/magicloops/blob/master/simpleloop.py
 
@@ -390,6 +391,7 @@ def run_the_models(data, models_to_run, date_col, response, features, dummy_list
                     # append metrics to list
                     res = res + [auc, f1] + precise + recall 
                     model_results.append(res)
+                    plot_precision_recall_n(true_labels, predicted_vals, m)
                 except Exception as e:
                     print(e, m, p)
         df = pd.DataFrame(model_results, columns = cols)
@@ -407,3 +409,39 @@ def choose_model(df, metric_col, asc=False):
     best_params = mean_by_group.sort_values(metric_col, ascending=asc).drop_duplicates('model')
     best_model = best_params.iloc[0][['model', 'parameters']]
     return best_params, best_model
+
+def plot_precision_recall_n(y_true, y_prob, model_name):
+    """
+    makes precision recals curves for given true and prediction values of y
+    inputs:
+        y_true: true values of y
+        y_prob: values of y predicted by our model
+        model_name: name of our model 
+    """
+    y_score = y_prob
+    precision_curve, recall_curve, pr_thresholds = precision_recall_curve(y_true, y_score)
+    precision_curve = precision_curve[:-1]
+    recall_curve = recall_curve[:-1]
+    pct_above_per_thresh = []
+    number_scored = len(y_score)
+    for value in pr_thresholds:
+        num_above_thresh = len(y_score[y_score>=value])
+        pct_above_thresh = num_above_thresh / float(number_scored)
+        pct_above_per_thresh.append(pct_above_thresh)
+    pct_above_per_thresh = np.array(pct_above_per_thresh)
+    
+    plt.clf()
+    fig, ax1 = plt.subplots()
+    ax1.plot(pct_above_per_thresh, precision_curve, 'b')
+    ax1.set_xlabel('percent of population')
+    ax1.set_ylabel('precision', color='b')
+    ax2 = ax1.twinx()
+    ax2.plot(pct_above_per_thresh, recall_curve, 'r')
+    ax2.set_ylabel('recall', color='r')
+    ax1.set_ylim([0,1])
+    ax1.set_ylim([0,1])
+    ax2.set_xlim([0,1])
+    
+    name = model_name
+    plt.title(name)
+    plt.show()
